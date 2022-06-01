c767924d920ae8892507cdff52275812f9f189a4
juanjose_calderon_kairosds <juanjose.calderon@kairosds.com>
2022-06-01 06:55:20 +0200

    style: add base css

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <button class="btn-raised-primary">Entrar</button>
+  <button class="btn-raised-primary">Salir</button>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
@@ -15,12 +16,4 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
