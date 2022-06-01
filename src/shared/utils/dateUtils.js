856d4d16337072a8111efca5925c19a9eaf4be2d
juanjose_calderon_kairosds <juanjose.calderon@kairosds.com>
2022-06-01 22:26:56 +0200

    feat: add time utils for calculation

---
+++ src/shared/utils/dateUtils.js
@@ -0,0 +1,19 @@
+export default class DateUtils {
+
+    static getFormatedTime({ timeNumber }) {
+        var sec_num = timeNumber / 1000;
+        var hours   = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+        if (hours   < 10) hours   = "0"+hours;
+        if (minutes < 10) minutes = "0"+minutes;
+        if (seconds < 10) seconds = "0"+seconds;
+
+        return hours + ':' + minutes + ':' + seconds;
+    }
+
+    static getTimeDifference({ date1, date2 }) { 
+        return new Date(date1).getTime() - new Date(date2).getTime();
+    }
+}
